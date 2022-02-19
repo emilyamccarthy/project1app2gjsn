9b36765b62303ae20f0b7dd5b748d47ffdd4de11
emilyamccarthy <46628784+emilyamccarthy@users.noreply.github.com>
2022-02-19 16:36:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var map = L.map('mapid').setView([45.513073847248585, -122.6559512530686], 12);
+var map = L.map('map').setView([45.515987177889805, -122.67676756785055], 10.5);
 
   // load a tile layer
- L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
- // load GeoJSON from an external file
+  // load GeoJSON from an external file
   $.getJSON("https://raw.githubusercontent.com/emilyamccarthy/project1app2gjsn/main/places.geojson",function(data){
     // add GeoJSON layer to the map once the file is loaded
     L.geoJson(data).addTo(map);
